f6b27b8699482e54f32180591d61b4f0c2962413
Syohei YOSHIDA <syohex@gmail.com>
2015-09-10 12:54:08 +0900

    Forbit some functions

--- elisp/init.el
+++ elisp/init.el
@@ -4,3 +4,19 @@
   (cond ((fboundp keyword) (substring-no-properties (describe-function keyword)))
         ((boundp keyword) (substring-no-properties (describe-variable keyword)))
         (t (format "Not found '%s'" keyword))))
+
+(defvar elbot-forbidden-functions
+  '(kill-emacs
+    save-buffers-kill-emacs
+
+    delete-file delete-directory
+
+    shell-command shell-command-to-string
+    call-process call-process-shell-command call-process-region
+    process-file process-file-shell-command
+
+    defun cl-defun defmacro cl-defmacro defsubst cl-defsubst setf))
+
+(dolist (fn elbot-forbidden-functions)
+  (fset fn `(lambda (&rest _args) ,(format "Can't use \"%s\" function. :scream:" fn))))
+(fset 'fset (lambda (&rest _args) "Can't use \"fset\" function. :scream:"))
