80ad88ef6a65c55cdf2d4bf1ead3514590ba99f5
Syohei YOSHIDA <syohex@gmail.com>
2015-09-10 20:55:16 +0900

    Update forbidden function list

--- elisp/init.el
+++ elisp/init.el
@@ -15,8 +15,28 @@
     call-process call-process-shell-command call-process-region
     process-file process-file-shell-command
 
+    insert-file insert-file-1 insert-file-contents insert-file-contents-literally
+    insert-file-literally
+
+    find-alternate-file find-alternate-file-other-window
+    find-file find-file--read-only find-file-at-point find-file-existing
+    find-file-literally find-file-literally-at-point find-file-name-handler
+    find-file-noselect find-file-noselect-1 find-file-other-frame find-file-other-window
+    find-file-read-args find-file-read-only find-file-read-only-other-frame
+    find-file-read-only-other-window
+
     defun cl-defun defmacro cl-defmacro defsubst cl-defsubst setf))
 
 (dolist (fn elbot-forbidden-functions)
-  (fset fn `(lambda (&rest _args) ,(format "Can't use \"%s\" function. :scream:" fn))))
-(fset 'fset (lambda (&rest _args) "Can't use \"fset\" function. :scream:"))
+  (fset fn `(lambda (&rest _args) ,(format "[ELBOT_ERROR] '%s'は使用できません. :scream:" fn))))
+
+(defvar elbot-forbidden-games
+  '(animate-birthday-present
+    blackbox mpuz 5x5 bubbles decipher dissociated-press dunnet hanoi gomoku
+    life landmark morse-region unmorse-region nato-region denato-region
+    pong snake tetris solitaire zone doctor))
+
+(dolist (fn elbot-forbidden-games)
+  (fset fn `(lambda (&rest _args) ,(format "[ELBOT_ERROR]仕事しろ :hamster:" fn))))
+
+(fset 'fset (lambda (&rest _args) "'fset'は使用できません"))
